1a721badf837106864d15d7210b9c6033fdd7fdc
lena <rublen77@gmail.com>
2018-04-29 14:23:23 +0300

    blocks_task: initial commit

--- part_5/carriage.rb
+++ part_5/carriage.rb
@@ -3,8 +3,40 @@ require_relative 'manufacturer'
 class Carriage
   include Manufacturer
 
-  attr_reader :type
-  def initialize
+  attr_reader :space, :type
+
+  def initialize(space = 0)
     @type = 'cargo'
+    @space = space.to_f
+    @counter = 0
+    validate! if space != 0
+  end
+
+  def available
+    space - counter
+  end
+
+  def taken
+    counter
+  end
+
+  def take_place(value = 1) # 1 - for passenger carriage
+    raise 'FAILED! There is no free seats/volume' if available < value
+    self.counter += value
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  protected
+  attr_accessor :counter
+
+  private
+  def validate!
+    raise 'FAILED! The spaciousness in the carriage should be a number greater than 0' unless space > 0
+    true
   end
 end
