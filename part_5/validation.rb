b11949be2c145930464231d3a8762463fe7ee533
lena <rublen77@gmail.com>
2018-05-22 14:04:21 +0300

    meta_tasks: accepted!

--- part_5/validation.rb
+++ part_5/validation.rb
@@ -1,43 +1,36 @@
 module Validation
   def self.included(base)
-    base.class_variable_set(:@@validated_attrs, [])
+    base.class_variable_set(:@@validations, [])
     base.extend(ClassMethods)
   end
 
   module ClassMethods
     def validate(attr, validation_type, validation_param = nil)
-      validated_attrs = class_variable_get(:@@validated_attrs)
-      validated_attrs << { attr => [validation_type, validation_param] }
-      class_variable_set(:@@validated_attrs, validated_attrs)
-    end
-
-    def new(*args)
-      super
-    rescue RuntimeError => e
-      puts "#{e.message} in class #{self}"
+      validations = class_variable_get(:@@validations)
+      validations << { attr => [validation_type, validation_param] }
+      class_variable_set(:@@validations, validations)
     end
   end
 
-  def presence(attr, truthness = nil)
-    attr = eval(attr.to_s)
-    attr && attr != ''
+  def presence(attr_value, _truthness = nil)
+    attr_value && attr_value != ''
   end
 
-  def format(attr, pattern)
-    eval(attr.to_s).to_s =~ pattern
+  def format(attr_value, pattern)
+    attr_value =~ pattern
   end
 
-  def type(attr, klass)
-    eval(attr.to_s).instance_of? klass
+  def type(attr_value, klass)
+    attr_value.instance_of? klass
   end
 
   def validate!
-    validation_var = self.class.class_variable_get :@@validated_attrs
-    return unless validation_var
-    validation_var.each do |item|
-      item.each_pair do |attr, validations|
-        raise TypeError, "/**Symbol was expected" unless validations[0].is_a? Symbol
-        raise "/**#{validations[0].capitalize}ErrorOfValitation of @#{attr}" unless send(validations[0], attr, validations[1])
+    validations = self.class.class_variable_get :@@validations
+    return unless validations
+    validations.each do |item|
+      item.each_pair do |attr, valids|
+        raise TypeError, "/**Symbol was expected" unless valids[0].is_a? Symbol
+        raise "/**#{valids[0].capitalize}ErrorOfValitation of @#{attr}" unless send(valids[0], instance_variable_get("@#{attr}".to_sym), valids[1])
       end
     end
   end
